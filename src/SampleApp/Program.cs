5913a8418335d70c80770ae216453d036830d2f4
심승용 <seungyong.shim@mirero.co.kr>
2020-12-11 13:42:28 +0900

    해결

--- src/SampleApp/Program.cs
+++ src/SampleApp/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using FluentAssertions;
 
-namespace SampleApp
+class Solution
 {
-    class Program
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d)
     {
-        static void Main(string[] args)
+        var queue = new Queue<int>(a);
+
+        for (int i = 0; i < d; i++)
         {
-            Console.WriteLine("Hello World!");
+            queue.Enqueue(queue.Dequeue());
         }
+
+        return queue.ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+        int n = 5;
+
+        int d = 4;
+
+        int[] a = { 1, 2, 3, 4, 5 };
+
+        rotLeft(a, d).Should().BeEquivalentTo(
+            new int[] { 5, 1, 2, 3, 4 }
+            );
+
     }
 }
